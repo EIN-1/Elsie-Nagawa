825a26c008fe49d851f0fa5a15f0b41e4d94f133
EIN-1 <198elsie@gmail.com>
2026-05-12 17:41:13 +0200

    modified:   index.html         modified:   script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function printLebenslauf() {
-    var lebenslaufUrl = "image/portfolio_doc.pdf"; // PDF file URL
-    console.log("Checking PDF existence:", lebenslaufUrl);
+function printKontaktMeUrlf() {
+    var KontaktMeUrl = "image/portfolio_doc.pdf"; // PDF file URL
+    console.log("Checking PDF existence:", KontaktMeUrl);
 
     // Check if the PDF file exists before opening it
-    fetch(lebenslaufUrl, { method: "HEAD" })
+    fetch(KontaktMeUrl, { method: "HEAD" })
         .then(response => {
             if (response.ok) {
                 console.log("PDF exists. Opening...");
-                window.open(lebenslaufUrl, "_blank"); // Open PDF in a new tab
+                window.open(KontaktMeUrl, "_blank"); // Open PDF in a new tab
             } else {
                 console.error("PDF not found. Redirecting to 404 page.");
                 window.location.href = "404.html"; // Redirect to 404 page
